4f7e3f0125f7c4cff625a171c47aabb409c466bf
Azarashiha <87087013+Azarashiha@users.noreply.github.com>
2022-11-26 01:44:29 +0900

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,370 @@
+from typing import Counter
+from bs4 import BeautifulSoup
+from bs4.element import Tag
+from feedparser.api import parse
+import sqlite3
+import sys,os,time,re,requests
+import csv,json
+import xmltodict
+import feedparser
+import tweepy
+import discord
+from discord.ext import tasks
+from selenium import webdriver
+import chromedriver_binary
+import time, selenium.webdriver
+from discordwebhook import Discord
+import datetime
+import subprocess
+
+
+def wgetc(url):
+    subprocess.run(["wget","-N",url],cwd=r"/Users/自身のファイル名")
+
+def e_xml(url):
+    response = requests.get(url,headers={'Cache-Control': 'no-cache'})
+    #成功200、
+    
+    #r = requests.get(url).content
+
+    if response.status_code== 200:
+        # jsonモジュールを使用する
+        #print("アクセス成功")
+        #文字化け解消↓
+        response.encoding = response.apparent_encoding
+        xml = response.text
+        
+        file = open('data/new.xml', 'w')
+        file.write(xml)
+        file.close()
+        print("new.xml complete!")
+
+
+def getRssFeedData():
+    url = 'http://localhost:7001/data/e.xml'#'https://www.data.jma.go.jp/developer/xml/feed/eqvol_l.xml'
+    wgetc(url)
+    # アクセスするrdfのURLを記載
+    RSS_URL = 'data/e.xml'#'data/eqvol_l.xml'#'http://www.data.jma.go.jp/developer/xml/feed/eqvol.xml'
+    #RSS_URL='http://www.data.jma.go.jp/developer/xml/feed/eqvol_l.xml'
+    xml = feedparser.parse(RSS_URL)
+    for entry in xml.entries:
+        if (entry.title == '震度速報'):
+            eid=entry.id
+            #print(entry.title)
+            break
+
+        if (entry.title == '震源・震度に関する情報'):
+            eid=entry.id
+            #print(entry.title)
+            break
+
+        eid=None
+    return eid
+
+# URLが更新されたかチェック
+def check():
+    rss_url = getRssFeedData()
+    #print(rss_url)
+    #rss_url='https://www.data.jma.go.jp/developer/xml/data/20220316143844_0_VXSE51_270000.xml'
+    #rss_url='https://www.data.jma.go.jp/developer/xml/data/20220316151110_0_VXSE53_010000.xml'
+    #rss_url='http://www.data.jma.go.jp/developer/xml/data/20211007134424_0_VXSE51_010000.xml'
+    #rss_url='https://www.gpvweather.com/jmaxml-view.php?k=%E9%9C%87%E6%BA%90%E3%83%BB%E9%9C%87%E5%BA%A6%E3%81%AB%E9%96%A2%E3%81%99%E3%82%8B%E6%83%85%E5%A0%B1&p=%E6%B0%97%E8%B1%A1%E5%BA%81&ym=2021-10&f=2021-10-07T13%3A45%3A22-20211007134523_0_VXSE53_010000.xml'
+    #rss_url='https://www.mk-mode.com/rails/jmaxml/2021/10/07/20211007135047_0_VXSE53_010000.xml'
+    path = 'data/latest_url.txt'
+    #print(rss_url)
+    if (rss_url == None):
+        print('新着情報はありませんでした 1')
+    else:
+        # latest_url.txtがなければ新規作成
+        if not os.path.isfile(path):
+            string = 'new file'
+            with open(path, mode='w') as file:
+                file.write(string)
+
+        local_url = ''
+        with open(path, mode='r') as file:
+            local_url = file.read()
+        #print(local_url)
+        # 新着情報があるかチェック
+        if (local_url == rss_url):
+            print('新着情報はありませんでした 2')
+            #pass
+        #情報が更新されていない場合はここで処理が終わります。
+        else:
+        
+            #lineNotify(rss_url)
+            with open(path, mode='w') as file:
+                string = rss_url
+                file.write(string)
+            print('新着情報があったので通知しました')
+            #print(rss_url)
+            e_xml(rss_url)
+            return True
+
+   
+def screen_shot():
+    driver = webdriver.Chrome()
+
+    driver.set_window_size(1920, 1080)
+
+    driver.get('http://localhost:8095/')
+    time.sleep(5)
+    driver.save_screenshot('data/images/screenshot.png')
+    driver.quit()
+
+def perse():
+    z=check()
+    if z==True:
+        savename = 'data/new.xml'
+        xml = open(savename, "r", encoding="utf-8").read()#XMLよみこみ
+        soup = BeautifulSoup(xml, 'xml')
+
+        # sqlite3にcsvを都度展開する
+        connection = sqlite3.connect(":memory:")
+        terminal = connection.cursor()
+
+
+        #sqlite memory area
+        terminal.execute("create table area(id integer primary key,area_name text,latitude real,longitude real);")
+        with open('jma_area.csv','rt')as f:
+            b = csv.reader(f)
+            for t in b:
+                terminal.execute('insert into area values (?,?,?,?);', t)
+            #参考：https://www.teamxeppet.com/python-sqlite3-intro/#toc4
+            #.mode csv は使えない　らしい...。
+
+        #print(terminal.fetchall)
+
+        #sqlite memory city
+        terminal.execute("create table city(id integer primary key,city_name text,latitude real,longitude real);")
+        with open('data/jma_city.csv','rt')as f:
+            b = csv.reader(f)
+            for t in b:
+                terminal.execute('insert into city values (?,?,?,?);', t)
+                #参考：https://www.teamxeppet.com/python-sqlite3-intro/#toc4
+                #.mode csv は使えない　らしい...。
+        connection.commit()
+        terminal.execute('select * from area;')
+
+        # 震源の座標を取得
+        if soup.find('jmx_eb:Coordinate') is not None:
+            epicenterStr = soup.find('jmx_eb:Coordinate').string
+
+            # 分割
+            epicenterCoord = re.split(r'(\-|\+|\/)', epicenterStr)
+            # epicenterに入れる
+            epicenter = {"type":"Feature",
+                        "properties": {"class":"epicenter"},
+                        "geometry":{
+                            "type":"Point",
+                            "coordinates":[float(epicenterCoord[3] + epicenterCoord[4]), float(epicenterCoord[1] + epicenterCoord[2])]
+                            }
+                        }
+            epc=epicenter
+            areaLevelFeatures = [epicenter]
+            cityLevelFeatures = [epicenter]
+    
+        else:
+            epicenter = {"type":"Feature",
+                        "properties": {"class":"epicenter"},
+                        "geometry":{
+                            "type":"Point",
+                            "coordinates":''
+                            }
+                        }
+            epc=epicenter
+            areaLevelFeatures = [epicenter]
+            cityLevelFeatures = [epicenter]
+
+
+        list_area=[]
+        list_city=[]
+        list_area.append(epc)
+        list_city.append(epc)
+
+
+        for p in soup.find_all('Pref'):
+            #県をしゅとく
+            #for a in p.find_all('Area'):
+            for a in p.find_all('Area'):
+                #print(a.text)            
+
+                for city in a.find_all('City'):
+                    #print(city)
+                    for m in city.find_all('MaxInt'):
+                        if m is not None:
+                            maxint = m.text
+                        else:
+                            maxint = ""
+                            print('error')
+                    try:
+                        for ccode in city.find_all('Code'):
+                            #print(maxint)
+                            terminal.execute("select latitude,longitude,city_name,id from city where id=" + ccode.text)
+                            row = terminal.fetchone()
+                            cityLevelFeatures={"type":"Feature","properties":{"class": maxint, "code": int(str(row[3])[:-2]), "name": row[2]},"geometry":{"type":"Point","coordinates": [row[1],row[0]]}}
+
+                            list_city.append(cityLevelFeatures)
+                        #print(list_city)
+                    except TypeError:
+                        code=city.find('Code')
+                        #print(code)
+                        terminal.execute("select latitude,longitude,city_name,id from city where id=" + code.text)
+                        row = terminal.fetchone()
+                        cityLevelFeatures={"type":"Feature","properties":{"class": maxint, "code": int(str(row[3])[:-2]), "name": row[2]},"geometry":{"type":"Point","coordinates": [row[1],row[0]]}}
+                        list_city.append(cityLevelFeatures)
+
+                #-----area がcity分のmaxint をとってきてしまうことを防ぐ。
+                for tag in a.findAll("City"):
+                    #tag.decompose()
+                    for t in city.find_all(['Code','MaxInt']):
+                        t.decompose()
+                    tag.decompose()
+                #print("-"*10)
+                #---------------
+                for m in a.find_all('MaxInt'):
+            
+                    if m is not None:
+                        maxint = m.text
+                        #print(maxint)
+                    else:
+                        maxint = ""
+                        print('error')
+             #print(cnt_area)
+                try:
+                    for c in a.find_all('Code'):
+                        #print(c.text)
+                        terminal.execute("select latitude,longitude,area_name,id from area where id=" + c.text)
+                        row = terminal.fetchone()
+                        #print(type(areaLevelFeatures))
+                        #print(type(row[3]))
+                        areaLevelFeatures={"type":"Feature","properties":{"class": maxint, "code": row[3], "name": row[2]},"geometry":{"type":"Point","coordinates": [row[1],row[0]]}}
+                        #print(areaLevelFeatures)
+                        list_area.append(areaLevelFeatures)
+                        #list_area.append(epc)
+                        #print(list_area)
+                except TypeError:
+                    code=a.find('Code')
+            
+                    terminal.execute("select latitude,longitude,area_name,id from area where id=" + code.text)
+                    row = terminal.fetchone()
+                    #print(type(row[3]))
+                    areaLevelFeatures={"type":"Feature","properties":{"class": maxint, "code": row[3], "name": row[2]},"geometry":{"type":"Point","coordinates": [row[1],row[0]]}}
+
+                
+                
+        colec='FeatureCollection'
+        #print(row)
+        areaLevelFeatureCollection = {"type":colec,"features":list_area}
+        cityLevelFeatureCollection = {"type":colec,"features":list_city}             
+        #print(areaLevelFeatureCollection)
+
+        file = open('data/jma_area.json', 'w')
+        json.dump(areaLevelFeatureCollection, file, ensure_ascii=False, indent=4, sort_keys=True, separators=(',',':'))
+        
+        file = open('data/jma_city.json', 'w')
+        json.dump(cityLevelFeatureCollection, file, ensure_ascii=False, indent=4, sort_keys=True, separators=(',',':'))
+        
+        
+        try:
+            h=soup.find('Hypocenter')
+            a=h.find("Area") 
+        
+            name=(a.find("Name")).text      
+            #print(name)
+            magunitude=(soup.find('jmx_eb:Magnitude')).text
+            head=soup.find("Head")
+            headline=head.find("Headline")
+            text=(headline.find("Text")).text
+            #print(text)
+            h=soup.find('Intensity')
+            a=h.find("Observation") 
+            maxint=(a.find("MaxInt")).text  
+
+            h=soup.find('Control')
+            a=h.find('Title').text
+            title=a
+
+            jma_text={
+            "title":title,
+            "magunitude":magunitude,
+            "name":name,
+            "text":text,
+            "maxint":maxint
+            }
+        except:
+            h=soup.find('Headline')
+            a=h.find('Text')
+            text=(a).text
+            h=soup.find('Control')
+            a=h.find('Title').text
+            title=a
+            h=soup.find('Observation')
+            a=h.find('MaxInt').text
+            maxint=a
+            
+            
+            jma_text={
+                "title":title,
+                "text":text,
+                "maxint":maxint,
+                "magunitude":"調査中",
+                "name":"調査中",
+            }
+        
+
+    
+        file = open('data/jma_text.json', 'w')
+        json.dump(jma_text, file, ensure_ascii=True, indent=4, sort_keys=True)
+
+        # 後処理
+        file.close()
+        terminal.close()
+        connection.close()
+        print('perce complete!')
+
+        screen_shot()
+        print('screen shot...')
+        #tweet()
+        #print('tw posts ...')
+        #new_msg()
+        print('ds posts ...!')
+        return 0
+    else:
+        return 1
+
+import sizecheck
+szl=[]
+sumszl=0
+while True:
+    start = time.time()
+    sum(range(50000000))
+    dt_now = datetime.datetime.now()
+    print(dt_now.strftime('%H:%M:%S'))
+    #perse()
+    path = 'data/eqvol_l.xml'
+    path2= 'data/new.xml'
+    #byte->
+    sz=os.path.getsize(path)+os.path.getsize(path2)
+
+    s=perse()
+    if s==0:
+        szl.append(sz)
+    else:
+        szl.append(0)
+    if 2880<len(szl):
+        del(szl[0])
+    for i in szl:
+        sumszl+=i
+    print(len(szl))
+    print('[一回あたり:'+sizecheck.convert_size(sz)+']')
+    print('[24時間あたり:'+sizecheck.convert_size(sumszl)+']')
+
+
+    # チェック※適宜変更
+    elapsed = time.time() - start
+    print(f"[処理時間:{elapsed:.3f}s]")  # 秒
+    #print(f"{elapsed * 1000:.0f}ms")  # ミリ秒
+
+    print("-"*30)
+    time.sleep(5)
---
+++ src/maprender-mbs.js
@@ -0,0 +1,365 @@
+//https://docs.mapbox.com/mapbox-gl-js/example/add-image/
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXphcmFzaGkiLCJhIjoiY2t0YmdibXczMXZwbzJubzBnZHI4Ym4zMCJ9.1C3RNiQqSioL1NkDSFE5Xg';
+    
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/azarashi/cktbgkxip5jml17n06wvzmgj9',
+    center: [139.7670516, 35.6811673],//仮数値
+    zoom: 7,//仮数値
+    //bounds:addpoint(),
+    projection: 'globe',
+    customAttribution: ['<a href="https://www.jma.go.jp/jma/index.html">震度情報:©︎気象庁</a>', '<a href="https://nlftp.mlit.go.jp/index.html">国土数値情報:©︎国土交通省</a>','<a href="https://twitter.com/nyaonearthquake?s=21">編集:©︎nyaonearthquake</a>'],
+
+});
+
+// コントロール関係表示
+map.addControl(new mapboxgl.NavigationControl());
+var citydata;
+//マウスクリック時にポップアップを出すか
+var popup = new mapboxgl.Popup({
+    closeButton: true
+});
+
+//ボタン要素を取得------
+let switchBtn = document.getElementsByTagName('button')[0];
+//表示・非表示を切り替える要素を取得
+let box = document.getElementById('box');
+
+//styleのdisplayを変更する関数
+let changeElement = (el)=> {
+
+  if(el.style.display==''){
+    el.style.display='none';
+  }else{
+    el.style.display='';
+  }
+
+}
+//=========================
+
+var url = 'jma_area.json'//'jma_area_e.json'//'https://example.com/x.json';
+$.getJSON(url, function(data){
+    areadata=data
+});
+var url = 'jma_city.json'//'jma_area_e.json'//'https://example.com/x.json';
+$.getJSON(url, function(data){
+    citydata=data
+});
+map.on("load", function () {
+    map.addControl(new mapboxgl.Minimap({
+center: [139.7670516, 35.6811673],//[areadata['features'][1]['geometry']['coordinates'][0], areadata['features'][1]['geometry']['coordinates'][1]],
+style: 'mapbox://styles/azarashi/cladgnrdz000315qmj9d3q6iu',
+zoom: 2,
+zoomLevels: []
+}), 'bottom-left');})
+
+
+//レスポンシブ(横640px)
+var mobile = $(window).width() < 540;
+    
+
+    
+map.on('load', function () {
+    
+//console.log('data : ', data);
+console.log('areaデータの数 '+areadata['features'].length)//geojson長さ
+console.log('cityデータの数 '+citydata['features'].length)//geojson長さ
+
+console.log(areadata['features'][0]['geometry']['coordinates'][0])
+
+imgfile='image'
+map.loadImage(`${imgfile}/epicenter.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('epicenter')) map.addImage('epicenter', image);
+    
+}),
+map.loadImage(`${imgfile}/int1.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('1')) map.addImage('1', image);
+}),
+map.loadImage(`${imgfile}/int2.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('2')) map.addImage('2', image);
+}),
+map.loadImage(`${imgfile}/int3.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('3')) map.addImage('3', image);
+}),
+map.loadImage(`${imgfile}/int4.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('4')) map.addImage('4', image);
+}),
+map.loadImage(`${imgfile}/int5l.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('5-')) map.addImage('5-', image);
+}),
+map.loadImage(`${imgfile}/int5h.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('5+')) map.addImage('5+', image);
+}),
+map.loadImage(`${imgfile}/int6l.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('6-')) map.addImage('6-', image);
+}),
+map.loadImage(`${imgfile}/int6h.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('6+')) map.addImage('6+', image);
+}),
+map.loadImage(`${imgfile}/int7.png`, (error, image) => {
+    if (error) throw error;
+    if (!map.hasImage('7')) map.addImage('7', image);
+}),
+
+
+map.addSource('data', {
+    'type': 'geojson',
+    'data': areadata,
+    maxzoom: 10,
+    });
+// ポイントソース設定
+map.addSource('vt', {
+    promoteId: "N03_007",
+    'type': 'vector',
+    'tiles': ['https://azarashiha.github.io/vt/{z}/{x}/{y}.pbf'],//['https://weatherbox.github.io/warning-area-vt/v2/{z}/{x}/{y}.pbf']
+    //"https://weatherbox.github.io/warning-area-vt/v2/{z}/{x}/{y}.pbf"
+    
+});
+//https://labs.mapbox.com/education/impact-tools/data-joins/
+for (const feature of citydata.features){
+    const id=feature.properties.code;
+    const c=feature.properties.class;
+    map.setFeatureState(
+        {
+            'source':'vt',
+            'sourceLayer':'city',
+            'id': id,
+        },
+        {
+            'class':c
+        }
+    )
+}
+map.addLayer({
+    'id': 'pref-line',
+    'type': 'fill',
+    'source': 'vt',
+    'source-layer': 'city',
+    'paint': {
+        //https://www.w3schools.com/css/css_colors_rgb.asp
+        'fill-color': [
+            'match',
+            ['feature-state','class'],
+            '1',
+            'rgba(171,223,244,0.7)',
+            '2',
+            'rgba(115,189,105,0.7)',
+            '3',
+            'rgba(227,227,80,0.7)',
+            '4',
+            'rgba(249,162,65,0.7)',
+            '5-',
+            'rgba(239,57,59,0.7)',
+            '5+',
+            'rgba(239,57,59,0.7)',
+            '6-',
+            'rgba(161,23,23,0.7)',
+            '6+',
+            'rgba(161,23,23,0.7)',
+            '7',
+            'rgba(56,1,13,0.7)',
+            'rgba(128,128,128,0.2)',
+
+        ],
+        'fill-opacity': 0.9,
+        'fill-outline-color': 'rgba(82,82,82, 0.4)'//'rgba(200, 100, 240, 1)'
+        }
+    
+    });
+
+//epicenterのみ先述    
+map.addLayer({
+    'id':'epicenter',
+    'type':'symbol',
+    'source':'data',
+    'filter': ['==', 'class', 'epicenter'],
+    'layout':{
+        'icon-image': 'epicenter',//`${symbol}`,
+        'icon-allow-overlap': true,
+        'icon-size': 1.5
+    },
+    //'filter': ['==', 'class', symbol]
+});
+
+    //参考：https://docs.mapbox.com/mapbox-gl-js/example/filter-markers/
+    for (const feature of areadata.features){
+    const symbol = feature.properties.class;
+    const layerID = `${symbol}`;
+    
+    
+    
+
+    if(!map.getLayer(layerID)){
+        console.log(layerID);
+        map.addLayer({
+            'id':`${layerID}`,
+            'type':'symbol',
+            'source':'data',
+            'filter': ['==', 'class', symbol],
+            'layout':{
+                'icon-image': `${symbol}`,
+                'icon-allow-overlap': true,
+                'icon-size': 0.5
+            },
+            //'filter': ['==', 'class', symbol]
+        });
+
+        
+        
+        
+    }
+
+    
+    
+    
+    }
+    
+    // 指定位置にズーム
+//const center=(data['features'][0]['geometry']['coordinates']);
+//console.log('center'+center);
+
+function addpoint(){
+    let array =[];
+    for(let i=0;i<=areadata['features'].length;i++){
+        //console.log(data['features'][i]['geometry']['coordinates']);
+        
+        let j=areadata['features'][i]['geometry']['coordinates']
+        array.push(j);
+        //console.log(array);
+        if (i==(areadata['features'].length-1)){
+            return array
+        }
+    }
+}
+//マーカーの位置に応じて最適なzoomを決める
+//console.log(addpoint())
+//map.fitBounds(addpoint(), {padding: 320,animate: false},);
+console.log(areadata['features'].length)
+var line = turf.lineString(addpoint());
+var bbox = turf.bbox(line); 
+if(areadata['features'].length>7){
+    map.fitBounds(bbox, {padding: 100,animate: false},)
+    console.log("7以上")
+}else if(areadata['features'].length>4){
+    map.fitBounds(bbox, {padding: 250,animate: false},)
+    console.log("4以上")    
+}else{//データ数によって見づらいならないように
+    map.fitBounds(bbox, {padding: 360,animate: false},)
+    //近すぎると見ずらいから   
+    console.log("else")
+}
+
+
+
+//以下試験的に書いたプログラムなので無視してください。
+//===================
+
+ // レイヤ設定
+ var Map_AddLayer = {
+    epicenter: "震源地",
+    '7': "震度7",
+    '6+': "震度6強",
+    '6-': "震度6弱",
+    '5+': "震度5強",
+    '5-': "震度5弱",
+    '4': "震度4",
+    '3': "震度3",
+    '2': "震度2",
+    '1': "震度1",
+    
+    
+    
+};
+
+
+// レイヤメニュー作成
+for (var i = 0; i < Object.keys(Map_AddLayer).length; i++) {
+    // レイヤID取得
+    var id = Object.keys(Map_AddLayer)[i];
+    // aタグ作成
+    var link = document.createElement("a");
+    link.href = "#";
+    // id追加
+    link.id = id;
+    // 名称追加
+    link.textContent = Map_AddLayer[id];
+
+    // 初期表示全て表示
+    link.className = "active";
+
+    // aタグクリック処理
+    link.onclick = function (e) {
+        // id取得
+        var clickedLayer = this.id;
+        e.preventDefault();
+        e.stopPropagation();
+
+        // ON/OFF状態取得
+        var visibility = map.getLayoutProperty(clickedLayer, "visibility");
+
+        // ON/OFF判断
+        if (visibility === 'visible') {
+            // クリックしたレイヤを非表示
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            // クリックしたレイヤを表示
+            map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+            this.className = 'active';
+        }
+    };
+
+    // レイヤメニューにレイヤ追加
+    //var layers = document.getElementById("menu");
+    //layers.appendChild(link);
+}
+
+
+
+
+
+//===================
+});
+
+
+
+map.on('mousemove', (e) => {
+    //zoomレベル確認
+    //console.log('zoom: ' + map.getZoom());
+    var features = map.queryRenderedFeatures(e.point);
+    // Limit the number of properties we're displaying for
+    // legibility and performance
+    const displayProperties = [
+        'type',
+        'properties',
+        'id',
+        'layer',
+        'source',
+        'sourceLayer',
+        'state'
+        ];
+        
+        const displayFeatures = features.map((feat) => {
+        const displayFeat = {};
+        displayProperties.forEach((prop) => {
+        displayFeat[prop] = feat[prop];
+        });
+        return displayFeat;
+        });
+        
+        // Write object as string with an indent of two spaces.
+        document.getElementById('features').innerHTML = JSON.stringify(
+        displayFeatures,
+        null,
+        2
+    );
+})
